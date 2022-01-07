9baaf9eccc86dbbde817d7edada429119c970086
Tayfun Yılmaz <76071161+ttayfunylmz@users.noreply.github.com>
2022-01-07 17:14:28 +0300

    Stack with Arrays

--- StackWithArrays/main.c
+++ StackWithArrays/main.c
@@ -1,74 +1,138 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 5
+#define STACK_SIZE 10
+#define EMPTY -1
 
-int arr[SIZE], top;
+#define TRUE 1
+#define FALSE 0
 
-//For adding data, we use PUSH function.
-void push(int data) {
-    if(top + 1 == SIZE) { //Top starts from -1. So "Top + 1" is the size of the array.
-        printf("Stack is full.");
+typedef int BOOL;
+//LIFO -> Last in First Out
+typedef struct tagStack {
+    int* pData; //For pData part of the Stack Abstract Data Type
+    int size; //Size of the data
+    int sp; //The indicator for the new entries and removal (Stack Pointer)
+}STACK;
+
+STACK* CreateStack(int size);
+void DestroyStack(STACK* pStack);
+
+BOOL Push(STACK* pStack, int data);
+BOOL IsFull(STACK* pStack);
+
+BOOL Pop(STACK* pStack, int* pData);
+BOOL Peek(STACK* pStack, int* pData);
+BOOL IsEmpty(STACK* pStack);
+
+//Instead of creating a new box in every case, we are making it in a function.
+STACK* CreateStack(int size) {
+    STACK* pStack = NULL;
+
+    pStack = (STACK*)malloc(sizeof(STACK));
+    if(NULL == pStack) {
+        fprintf(stderr, "Can NOT allocate memory for the STACK structure");
+        exit(EXIT_FAILURE);
     }
-    else { //If stack is not FULL :
-        //First increase TOP and then we can increase the new data.
-        top += 1;
-        arr[top] = data;
+    pStack->pData = (int*)malloc(sizeof(int) * size);
+    if(NULL == pStack->pData) {
+        fprintf(stderr, "Can NOT allocate memory for the STACK DATA");
+        exit(EXIT_FAILURE);
+    }
+    pStack->size = size;
+    //Applying second assumption :
+    pStack->sp = EMPTY; //SP points first data top REMOVE (-1)
+
+    return pStack;
+}
+
+//By that function, we free our data which means we are destroying them from the memory.
+void DestroyStack(STACK* pStack) {
+    if(NULL != pStack) {
+        free(pStack->pData);
+        free(pStack);
+    }
+    else {
+        printf("Your pStack is already CLEANED-UP!");
+        exit(EXIT_FAILURE);
     }
 }
 
-//For removing data, we use POP function.
-void pop() {
-    if(top == -1){ //If TOP points -1, this means stack is empty.
-        printf("Stack is already empty.");
-    }
-    else { //If stack is not EMPTY :
-        top -= 1; //Decreasing TOP. And by that, removing the data.
-    }
-}
-//Peek function just peeks the data. It does NOT remove it.
-int peek() {
-    return arr[top]; //Traverses the whole array and shows the TOP.
-}
-
-//For printing the list, we use printList function.
-void printList() {
-    for(int i=0; i<top+1; ++i) {
-        printf("%d ", arr[i]);
-    }
-    printf("==> TOP");
-}
-
-int main()
-{
-    int choice;
-    top = -1;
-
-    while(1==1) {
-        printf("\n1-Push");
-        printf("\n2-Pop");
-        printf("\n3-Peek\n");
-        printf("Enter 1 for Push, 2 for Pop and 3 for Peek\n");
-        scanf("%d", &choice);
-
-        switch(choice) {
-            case 1:
-                printf("Enter a number to add : ");
-                scanf("%d", &choice);
-                push(choice);
-                printList();
-                break;
-            case 2:
-                pop();
-                printList();
-                break;
-            case 3:
-                printf("Top number is : %d", peek());
-                break;
-            default:
-                printf("Enter a valid number!");
-                break;
+//To add data to our Stack, we must use our Push function.
+BOOL Push(STACK* pStack, int data) {
+    if(NULL != pStack) {
+        if(TRUE == IsFull(pStack)) {
+            printf("You can NOT push more. Stack is FULL. STACK OVERFLOW.");
+            return FALSE;
         }
+        //First, we increase the sp and then we assign a value to the data. Because we are adding data.
+        pStack->sp++;
+        pStack->pData[pStack->sp] = data;
+        return TRUE;
     }
-    return 0;
+    return FALSE;
+}
+
+BOOL IsFull(STACK* pStack) {
+    //We used TERNARY OPERATOR to make sure the capacity is FULL.
+    return (pStack->sp == pStack->size) ? TRUE : FALSE;
+}
+
+//To remove data from our Stack, we must use our Pop function.
+BOOL Pop(STACK* pStack, int* pData) {
+    if(NULL != pStack) {
+        if(TRUE == IsEmpty(pStack)) {
+            printf("You can NOT pull. Stack is EMPTY. STACK UNDERFLOW.");
+            return FALSE;
+        }
+        //First, we assign a value in the data and then we decrease it. Because we are removing data.
+        *pData = pStack->pData[pStack->sp];
+        pStack->sp--;
+        return TRUE;
+    }
+    return FALSE;
+}
+
+//We are not adding or removing data in Peek function. It only shows the PEEK DATA.
+BOOL Peek(STACK* pStack, int* pData) {
+    if(NULL != pStack) {
+        if(TRUE == IsEmpty(pStack)) {
+            printf("You can NOT pull. Stack is EMPTY. STACK UNDERFLOW.");
+            return FALSE;
+        }
+        *pData = pStack->pData[pStack->sp];
+        return TRUE;
+    }
+    return FALSE;
+}
+//We used TERNARY OPERATOR to make our Stack is empty.
+BOOL IsEmpty(STACK* pStack) {
+    return (pStack->sp == EMPTY) ? TRUE : FALSE;
+}
+
+int main() {
+    STACK* pStack1;
+    int i, data;
+
+    pStack1 = CreateStack(STACK_SIZE); //We defined STACK_SIZE as 10.
+
+    for(i=0; i<STACK_SIZE; ++i){
+        printf("STACK Data : %d\n",i+1);
+
+        if(FALSE == Push(pStack1, i+1))
+            printf("Can NOT Push data  %d\n", i+1);
+    }
+    printf("-----------------------\n");
+
+    if(TRUE == Peek(pStack1, &data))
+        printf("PEEKed data from STACK (TOP VALUE) : %d\n", data);
+    printf("-----------------------\n");
+
+    for(i=0; i<STACK_SIZE; ++i)
+        if(TRUE == Pop(pStack1, &data))
+            printf("POPed data from STACK : %d\n", data);
+
+    DestroyStack(pStack1);
+
+    return EXIT_SUCCESS; //This comes from <stdlib.h>
 }
